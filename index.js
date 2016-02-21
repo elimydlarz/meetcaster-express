ae418e2c5614f3df6101cd075f4304cad7634bf0
Eli Mydlarz <eli.mydlarz@gmail.com>
2016-02-21 19:01:46 +1100

    Updating start log to reflect port change.

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', function (req, res) {
 });
 
 app.listen(5000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 5000!');
 });
